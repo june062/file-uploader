13cd84f43ea2d165166e28b1fd60ae5da41eb309
Juneid Teklu <te.juneid@gmail.com>
2024-11-11 11:12:40 -0600

    Finish configuring passport

--- src/config/passportConf.js
+++ src/config/passportConf.js
@@ -1,24 +1,49 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const prisma = require("../models/queries");
+const crypt = require("bcryptjs");
 
 passport.use(
   new LocalStrategy(async function (username, password, done) {
-    const user = await prisma.user.findUnique({
-      where: {
-        usernmae: username,
-      },
-    });
-    if (!user) {
-      done(/* error, null? */);
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          usernmae: username,
+        },
+      });
+      if (!user) {
+        return done(null, false, { message: "Wrong username" });
+      } else {
+        const hashedPassword = await crypt.hash(password, 12);
+        const match = await crypt.compare(user.password, hashedPassword);
+        if (match) {
+          return done(true, user);
+        } else {
+          return false, null, { message: "Wrong password" };
+        }
+      }
+    } catch (error) {
+      return done(error);
     }
   })
 );
-/* Authenticates the given username and password and passes authenticated user to serialize function */
 
-/* serialize; */
-/* Adds user info to session passport object */
+passport.serializeUser((user) => {
+  return done(null, user.id);
+});
 
-/* deserialize; */
-
-/* Retrieves active user from our db and stores that user as req.user object */
+passport.deserializeUser((userID) => {
+  try {
+    const user = prisma.user.findUnique({
+      where: {
+        id: userID,
+      },
+    });
+    if (!user) {
+      done(null, false);
+    }
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
